39a11406baa8469a7b0b6c61fc943f8f7e99e391
higor335 <higor-lopo@hotmail.com>
2024-05-26 18:53:29 -0400

    feat: :art: correção de interface - redisposição dos elementos e divisão dos objetos
    adicionado scroll lateral para visualização dos elementos e inserção de barras horizontais para dividir documentos

--- form.py
+++ form.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import messagebox
-from datetime import datetime, timedelta
+from datetime import datetime
 import csv
 
 class OrdemServico:
@@ -25,10 +26,11 @@ class GerenciadorOrdensServico:
         self.ordens_servico.append(ordem_servico)
 
     def ordens_servico_pendentes(self):
-        return [ordem for ordem in self.ordens_servico if ordem.entrega is None] 
-    
+        return [ordem for ordem in self.ordens_servico if ordem.entrega is None]
+
     def ordens_servico_concluidas(self):
         return [ordem for ordem in self.ordens_servico if ordem.entrega is not None]
+    
 
 def salvar_ordens_servico(filename, ordens_servico):
     try:
@@ -36,7 +38,6 @@ def salvar_ordens_servico(filename, ordens_servico):
             writer = csv.writer(file, delimiter=';')
             writer.writerow(["Ordem", "Número da Ordem", "Recebimento", "Direcionado para", "Direcionamento", "Prazo Recebimento", "Prazo Devolução", "Entrega"])
             for ordem in ordens_servico:
-                # Formatando as datas para o formato desejado
                 recebimento = ordem.recebimento.strftime("%d/%m/%Y %H:%M")
                 prazo_recebimento = ordem.prazo_recebimento.strftime("%d/%m/%Y")
                 prazo_devolucao = ordem.prazo_devolucao.strftime("%d/%m/%Y")
@@ -45,19 +46,16 @@ def salvar_ordens_servico(filename, ordens_servico):
     except Exception as e:
         messagebox.showerror("Erro", f"Erro ao salvar arquivo: FECHE O ARQUIVO EXCEL: ordens_servico.")
 
-
 def carregar_ordens_servico(filename):
     ordens_servico = []
     try:
         with open(filename, mode='r') as file:
             reader = csv.reader(file, delimiter=';')
-            next(reader)  # Pular o cabeçalho
-            
+            next(reader)
             for row in reader:
-                if len(row) == 8:  
+                if len(row) == 8:
                     nome, numero, recebimento, direcionado_para, direcionamento, prazo_recebimento, prazo_devolucao, entrega = row
                     if prazo_recebimento and prazo_devolucao:
-                        # Convertendo as datas do formato do arquivo para o formato Python
                         recebimento = datetime.strptime(recebimento, "%d/%m/%Y %H:%M")
                         prazo_recebimento = datetime.strptime(prazo_recebimento, "%d/%m/%Y").date()
                         prazo_devolucao = datetime.strptime(prazo_devolucao, "%d/%m/%Y").date()
@@ -71,7 +69,6 @@ def carregar_ordens_servico(filename):
         return []
     return ordens_servico
 
-
 def inserir_ordem():
     nome = entry_nome.get()
     numero = entry_numero.get()
@@ -82,26 +79,18 @@ def inserir_ordem():
     if not all([nome, numero, direcionado_para, prazo_recebimento, prazo_devolucao]):
         messagebox.showerror("Erro", "Todos os campos devem ser preenchidos.")
         return
-    
+
     try:
         prazo_recebimento = datetime.strptime(prazo_recebimento, "%d/%m/%Y").date()
         prazo_devolucao = datetime.strptime(prazo_devolucao, "%d/%m/%Y").date()
     except ValueError:
         messagebox.showerror("Erro", "Formato de data inválido. Use o formato dd/mm/aaaa.")
         return
-    
-    
+
     nova_ordem = OrdemServico(nome, numero, datetime.now(), direcionado_para, datetime.now(), prazo_recebimento, prazo_devolucao)
     gerenciador.inserir_ordem_servico(nova_ordem)
     salvar_ordens_servico("ordens_servico.csv", gerenciador.ordens_servico)
 
-    for label in labels_ordens:
-        label.destroy()  # Remove o label da interface gráfica
-    for botao in botoes_recebido:
-        botao.destroy()  # Remove o botão da interface gráfica
-    lista_ordens.delete(0, tk.END)
-
-    # Limpar os campos de entrada e atualizar a lista de ordens pendentes na interface
     entry_nome.delete(0, tk.END)
     entry_numero.delete(0, tk.END)
     entry_direcionado_para.delete(0, tk.END)
@@ -114,69 +103,67 @@ def inserir_ordem():
 def marcar_recebido(ordem):
     ordem.entregar()
     salvar_ordens_servico("ordens_servico.csv", gerenciador.ordens_servico)
-    listar_ordens_concluidas()
     listar_ordens_pendentes()
+    listar_ordens_concluidas()
     messagebox.showinfo("Sucesso", "Ordem de Serviço marcada como recebida!")
-    
 
 def listar_ordens_pendentes():
-    global labels_ordens  # Declare a lista global para armazenar os labels
-    global botoes_recebido  # Declare a lista global para armazenar os botões
-    global button_pendentes, button_concluidas
-    
+    global labels_ordens, botoes_recebido
+
     button_pendentes.config(state=tk.DISABLED)
     button_concluidas.config(state=tk.NORMAL)
 
-    lista_ordens.delete(0, tk.END)
-
-    y_position = 190  
-
+    for label in labels_ordens:
+        label.destroy()
+    for botao in botoes_recebido:
+        botao.destroy()
     labels_ordens = []
     botoes_recebido = []
 
-    for botao in botoes_recebido:
-        botao.destroy()  
-    lista_ordens.delete(0, tk.END)
+    canvas_pendentes.yview_moveto(0)
+    canvas_pendentes.delete("all")
 
-    if labels_ordens:
-        for label in labels_ordens:
-            label.destroy()  
-        labels_ordens = []  
-    if botoes_recebido:
-        for botao in botoes_recebido:
-            botao.destroy()  
-        botoes_recebido = []  
+    y_position = 0
 
-    lista_ordens.delete(0, tk.END)  
+    def adicionar_linha(canvas, y_position):
+        linha = canvas.create_line(0, y_position + 39, 800, y_position + 39, fill="gray")
+        return linha
+
+    linhas = []  # Lista para armazenar os IDs das linhas horizontais
 
     for ordem in gerenciador.ordens_servico_pendentes():
         text = f"{ordem.nome} N°{ordem.numero} - Com: {ordem.direcionado_para}  - Prazo: {ordem.prazo_recebimento.strftime('%d/%m/%Y')}"
 
-        # Criar um Label para o texto
-        label_ordem = tk.Label(root, text=text)
-        label_ordem.place(x=80, y=y_position+10)  # Defina as coordenadas x e y desejadas para o texto
-        labels_ordens.append(label_ordem)  # Adicione o label à lista de labels
+        label_ordem = tk.Label(canvas_pendentes, text=text, anchor="w")
+        label_ordem_window = canvas_pendentes.create_window(80, y_position + 8, anchor="nw", window=label_ordem)
+        labels_ordens.append(label_ordem)
 
-        # Criar botão para cada ordem
-        botao_recebido = tk.Button(root, text="Recebido", command=lambda o=ordem: marcar_recebido(o))
-        botao_recebido.place(x=10, y=y_position+8)  # Defina as coordenadas x e y desejadas para o botão
-        botoes_recebido.append(botao_recebido)  # Adicione o botão à lista de botões
+        botao_recebido = tk.Button(canvas_pendentes, text="Recebido", command=lambda o=ordem: marcar_recebido(o))
+        botao_recebido_window = canvas_pendentes.create_window(10, y_position + 6, anchor="nw", window=botao_recebido)
+        botoes_recebido.append(botao_recebido)
 
-        # Ajuste a posição y para o próximo texto e botão
-        y_position += 40  # Ajuste a posição y conforme necessário para separar os elementos
+        linha = adicionar_linha(canvas_pendentes, y_position)
+        linhas.append(linha)
+
+        y_position += 40
+
+    canvas_pendentes.config(scrollregion=canvas_pendentes.bbox("all"))
+    frame_canvas_pendentes.grid(row=9, columnspan=2, sticky="nsew")
+    lista_ordens.grid_forget()
 
 def listar_ordens_concluidas():
-    global labels_ordens
-    global botoes_recebido
-    global button_pendentes, button_concluidas
-    
+    global labels_ordens, botoes_recebido
+
     button_pendentes.config(state=tk.NORMAL)
     button_concluidas.config(state=tk.DISABLED)
-    
+
     for label in labels_ordens:
-        label.destroy()  # Remove o label da interface gráfica
+        label.destroy()
     for botao in botoes_recebido:
-        botao.destroy()  # Remove o botão da interface gráfica
+        botao.destroy()
+    labels_ordens = []
+    botoes_recebido = []
+
     lista_ordens.delete(0, tk.END)
 
     ordens_concluidas = sorted(gerenciador.ordens_servico_concluidas(), key=lambda x: x.entrega, reverse=True)
@@ -184,11 +171,14 @@ def listar_ordens_concluidas():
     for ordem in ordens_concluidas:
         lista_ordens.insert(tk.END, f"{ordem.nome} N°{ordem.numero} - Resolvido Por: {ordem.direcionado_para} - Data de Entrega: {ordem.entrega.strftime('%d/%m/%Y')}")
 
-# Configuração da interface
+    frame_canvas_pendentes.grid_forget()
+    lista_ordens.grid(row=9, columnspan=2, sticky="nsew")
+
 root = tk.Tk()
 root.title("Gerenciador de Ordens de Serviço")
 
-# Widgets
+labels_ordens = []
+botoes_recebido = []
 
 label_dev = tk.Label(root, text="Feito por: Higor Vital Lopo", font=("Helvetica", 8, "italic"))
 
@@ -210,12 +200,18 @@ entry_prazo_devolucao = tk.Entry(root)
 button_inserir = tk.Button(root, text="Inserir Ordem de Serviço", command=inserir_ordem)
 
 label_selecionar = tk.Label(root, text="Selecionar Opção:")
-button_pendentes = tk.Button(root, text="Listar Ordens de Serviço Pendentes", command=listar_ordens_pendentes, state=tk.DISABLED)
+button_pendentes = tk.Button(root, text="Listar Ordens de Serviço Pendentes", command=listar_ordens_pendentes)
 button_concluidas = tk.Button(root, text="Listar Ordens de Serviço Concluídas", command=listar_ordens_concluidas)
 
-lista_ordens = tk.Listbox(root, width=100, height=20)
+frame_canvas_pendentes = tk.Frame(root)
+canvas_pendentes = tk.Canvas(frame_canvas_pendentes, width=800, height=400, bg="white")
+scrollbar_pendentes = tk.Scrollbar(frame_canvas_pendentes, orient="vertical", command=canvas_pendentes.yview)
+canvas_pendentes.config(yscrollcommand=scrollbar_pendentes.set)
 
-# Layout
+canvas_pendentes.grid(row=0, column=0)
+scrollbar_pendentes.grid(row=0, column=1, sticky="ns")
+
+lista_ordens = tk.Listbox(root, width=100, height=20)
 
 label_dev.grid(row=8, column=0, sticky="w")
 
@@ -234,20 +230,17 @@ entry_prazo_recebimento.grid(row=3, column=1)
 label_prazo_devolucao.grid(row=4, column=0, sticky="e")
 entry_prazo_devolucao.grid(row=4, column=1)
 
-button_inserir.grid(row=5, columnspan=2) 
+button_inserir.grid(row=5, columnspan=2)
 label_selecionar.grid(row=6, column=0, sticky="e")
 button_pendentes.grid(row=6, column=1, sticky="w")
-button_concluidas.grid(row=8, column=1, sticky="w")
+button_concluidas.grid(row=7, column=1, sticky="w")
 
-lista_ordens.grid(row=10, columnspan=2)
+frame_canvas_pendentes.grid(row=9, columnspan=2, sticky="nsew")
+lista_ordens.grid(row=9, columnspan=2, sticky="nsew")
 
-# Instanciando o gerenciador
 gerenciador = GerenciadorOrdensServico()
-
-# Carregar ordens de serviço do arquivo CSV
 gerenciador.ordens_servico = carregar_ordens_servico("ordens_servico.csv")
 
-
 listar_ordens_pendentes()
-# Rodando a aplicação
-root.mainloop() 
+
+root.mainloop()
